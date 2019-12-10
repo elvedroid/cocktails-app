58afc6bfaaa892fa6f341d2361c12e89dddcaa24
Elvedin Selimoski <elvedin.selimoski@sanitas.com>
2019-12-10 13:57:45 +0100

    Drink details

--- lib/feature/drink_details/drink_details_view.dart
+++ lib/feature/drink_details/drink_details_view.dart
@@ -50,88 +50,197 @@ Widget _buildResults(DrinkDetailsBloc bloc) {
 }
 
 Widget _buildDrinkDetails(List<Drink> results) {
+  var drink = results[0];
+
   return Container(
     color: Color(0xffF5F2E8),
     child: ListView(children: <Widget>[
-      Container(
-        color: Color(0xffF5F2E8),
-        child: FadeInImage.assetNetwork(
-            height: 250,
-            placeholderScale: 5,
-            fit: BoxFit.cover,
-            placeholder: 'images/cocktail_anim.gif',
-            image: results[0].strDrinkThumb),
-      ),
-      Container(
-        color: Color(0x88000000),
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Center(
-            child: Text(results[0].strDrink,
+      _buildDetailImageCard(drink),
+      _buildDrinkInfoCard(drink),
+      _buildIngredientsCard(drink),
+      _buildDrinkBarsInfo(drink),
+    ]),
+  );
+}
+
+Card _buildDetailImageCard(Drink drink) {
+  return Card(
+    color: Color(0xffF5F2E8),
+    child: FadeInImage.assetNetwork(
+        height: 220,
+        placeholderScale: 5,
+        fit: BoxFit.cover,
+        placeholder: 'images/cocktail_anim.gif',
+        image: drink.strDrinkThumb),
+  );
+}
+
+Card _buildDrinkInfoCard(Drink drink) {
+  return Card(
+    margin: const EdgeInsets.fromLTRB(16, 16, 8.0, 16),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        Row(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: Text(
+                'Category:',
                 style: TextStyle(
-                  decoration: TextDecoration.none,
-                  color: Colors.white,
-                  fontSize: 28,
-                )),
-          ),
-        ),
-      ),
-      Container(
-        child: Padding(
-          padding: const EdgeInsets.fromLTRB(8.0, 36, 8.0, 0),
-          child: Text('Category: ' + results[0].strCategory,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+            Text(
+              drink.strCategory,
               style: TextStyle(
-                decoration: TextDecoration.none,
                 color: Colors.black,
-                fontSize: 18,
-              )),
+              ),
+            ),
+          ],
         ),
-      ),
-      Container(
-        child: Padding(
-          padding: const EdgeInsets.fromLTRB(8.0, 8.0, 8.0, 0),
-          child: Text('Glass: ' + results[0].strGlass,
+        Row(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: Text(
+                'Glass:',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+            Text(
+              drink.strGlass,
               style: TextStyle(
-                decoration: TextDecoration.none,
                 color: Colors.black,
-                fontSize: 18,
-              )),
+              ),
+            ),
+          ],
         ),
-      ),
-      Container(
-        child: Padding(
-          padding: const EdgeInsets.fromLTRB(8.0, 8.0, 8.0, 0),
-          child: Text(
-              'Alcoholic: ' +
-                  (results[0].strAlcoholic == 'Alcoholic' ? 'yes' : 'no'),
-              style: TextStyle(
-                decoration: TextDecoration.none,
-                color: Colors.black,
-                fontSize: 18,
-              )),
-        ),
-      ),
-      Card(
-        child: Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
-          Container(
-            child: Padding(
-              padding: const EdgeInsets.fromLTRB(8.0, 8.0, 8.0, 0),
-              child: Text('Ingredients',
-                  style: TextStyle(
-                    decoration: TextDecoration.none,
-                    color: Colors.black,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18,
+        Row(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: Text(
+                'Alcoholic: ',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+            (drink.strAlcoholic == 'Alcoholic'
+                ? Icon(
+                    Icons.check,
+                    color: Colors.lightGreen,
+                  )
+                : Icon(
+                    Icons.close,
+                    color: Color(0xfff2003c),
                   )),
+          ],
+        ),
+      ],
+    ),
+  );
+}
+
+Card _buildIngredientsCard(Drink drink) {
+  var ingredients = drink.strIngredients;
+  var measurements = drink.strMeasures;
+  var instructions = drink.strInstructions;
+  return Card(
+    margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.fromLTRB(
+            8,
+            16,
+            8,
+            4,
+          ),
+          child: Text(
+            "Ingredients",
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
             ),
           ),
-          ListView.builder(
-              itemCount: 0,
-              itemBuilder: (context, index) => Text(
-                  results[0].strIngredients[index],
-                  style: TextStyle(color: Colors.black)))
-        ]),
-      ),
-    ]),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(
+            vertical: 0,
+            horizontal: 8,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: List<int>.generate(ingredients.length, (i) => i + 1)
+                .map((index) => Text(
+                      ingredients[index - 1] +
+                          (measurements.length > index - 1
+                              ? " - " + measurements[index - 1]
+                              : ""),
+                      style: TextStyle(color: Colors.black),
+                    ))
+                .toList(),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(
+            8,
+            16,
+            8,
+            4,
+          ),
+          child: Text(
+            "Instructions",
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(
+            8,
+            0,
+            8,
+            16,
+          ),
+          child: Text(
+            instructions,
+            style: TextStyle(color: Colors.black),
+          ),
+        ),
+      ],
+    ),
+  );
+}
+
+Padding _buildDrinkBarsInfo(Drink drink) {
+  return Padding(
+    padding: const EdgeInsets.all(8.0),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        _buildDrinkPerBarInfo(drink),
+      ],
+    ),
+  );
+}
+
+Card _buildDrinkPerBarInfo(Drink drink) {
+  return Card(
+    child: Text("Bar1"),
   );
 }
